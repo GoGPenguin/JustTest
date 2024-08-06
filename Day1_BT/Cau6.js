5da6a631cc263b181551e33dbb399e7997adab2e
Nguyen Thai Ngoc Thao <ngocthaodana@gmail.com>
2024-08-06 18:14:02 +0700

    Refactor: func standsFor

--- Day1_BT/Cau6.js
+++ Day1_BT/Cau6.js
@@ -1,13 +1,11 @@
 //* Viết hàm viết tắt câu (VD: Hello anh em -> HAE)
 
-function standsFor1(s) {
+function standsFor(s) {
     let arr = s.split(' ');
     // split chuỗi s
-    let ss = "";
-    for (let word of arr) ss += word[0]; // lấy kí tự đầu của mỗi từ
-    return ss.toUpperCase();
+    return arr.map(item => item[0]).join('').toUpperCase();
 }
 
-console.log(standsFor1('Hello anh em'));
-console.log(standsFor1('Good morning Baby'));
-console.log(standsFor1('I love you'));
+console.log(standsFor('Hello anh em'));
+console.log(standsFor('Good morning Baby'));
+console.log(standsFor('I love you'));
